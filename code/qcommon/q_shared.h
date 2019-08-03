b799f771823f59d7435360a753f08d23521da4fc
Brian Cullinan <megamindbrian@gmail.com>
2019-08-02 17:07:53 -0700

    Update game info

--- code/qcommon/q_shared.h
+++ code/qcommon/q_shared.h
@@ -27,16 +27,16 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // A user mod should never modify this file
 
 #ifdef STANDALONE
-  #define PRODUCT_NAME				"iofoo3"
-  #define BASEGAME					"foobar"
-  #define CLIENT_WINDOW_TITLE		"changeme"
-  #define CLIENT_WINDOW_MIN_TITLE	"changeme2"
-  #define HOMEPATH_NAME_UNIX		".foo"
-  #define HOMEPATH_NAME_WIN			"FooBar"
+  #define PRODUCT_NAME				"planetquake"
+  #define BASEGAME					"base"
+  #define CLIENT_WINDOW_TITLE		"Planet Quake"
+  #define CLIENT_WINDOW_MIN_TITLE	"Planet Quake"
+  #define HOMEPATH_NAME_UNIX		".quake"
+  #define HOMEPATH_NAME_WIN			"PlanetQuake"
   #define HOMEPATH_NAME_MACOSX		HOMEPATH_NAME_WIN
 //  #define STEAMPATH_NAME			"Foo Bar"
 //  #define STEAMPATH_APPID         ""
-  #define GAMENAME_FOR_MASTER		"foobar"	// must NOT contain whitespace
+  #define GAMENAME_FOR_MASTER		"base"	// must NOT contain whitespace
   #define CINEMATICS_LOGO		"foologo.roq"
   #define CINEMATICS_INTRO		"intro.roq"
 //  #define LEGACY_PROTOCOL	// You probably don't need this for your standalone game
