18baa5a1db7ba8cbf827f1d0db00bd11d4f559e9
Brian Cullinan <megamindbrian@gmail.com>
2019-09-12 21:05:03 -0700

    Created trigger_exec entity for running any arbitrary command.

--- code/game/g_spawn.c
+++ code/game/g_spawn.c
@@ -152,6 +152,8 @@ void SP_target_kill (gentity_t *ent);
 void SP_target_position (gentity_t *ent);
 void SP_target_location (gentity_t *ent);
 void SP_target_push (gentity_t *ent);
+void SP_target_exec (gentity_t *ent);
+
 
 void SP_light (gentity_t *self);
 void SP_info_null (gentity_t *self);
@@ -228,6 +230,7 @@ spawn_t	spawns[] = {
 	{"target_position", SP_target_position},
 	{"target_location", SP_target_location},
 	{"target_push", SP_target_push},
+	{"target_exec", SP_target_exec},
 
 	{"light", SP_light},
 	{"path_corner", SP_path_corner},
