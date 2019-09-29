ce165e4c37907d1fed4ff3101d31ab614fc13b24
Brian Cullinan <megamindbrian@gmail.com>
2019-09-29 13:15:44 -0700

    working teleporter

--- code/renderergl2/tr_cmds.c
+++ code/renderergl2/tr_cmds.c
@@ -338,6 +338,12 @@ void RE_BeginFrame( stereoFrame_t stereoFrame, int world ) {
 	if ( !tr.registered ) {
 		return;
 	}
+	
+	if(numGlobalWorlds > 1
+	   && Q_stricmp(globalWorlds[world].world->name, tr.world->name)) {
+		R_IssuePendingRenderCommands();
+		tr.world = globalWorlds[world].world;
+	}
 
 	glState.finishCalled = qfalse;
 
